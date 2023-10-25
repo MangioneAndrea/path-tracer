b70074153938276fdfb502ad4bf0446cc53e309a
Andrea Mangione <andrea@mangione.dev>
2023-10-25 16:00:34 +0200

    noop

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::sync::mpsc::Sender;
 use std::sync::Arc;
 use std::thread;
@@ -12,11 +13,11 @@ pub(crate) mod cornell_box;
 
 const ITERATIONS: usize = 32;
 
-pub trait Scene {
+pub trait Scene: Sync + Send {
     fn compute_color(&self, camera: &Vec3, d: &Vec3, rng: &mut rand::rngs::ThreadRng) -> Color;
 
     fn get_pixels<const W: usize, const H: usize, const STEP: usize>(
-        &mut self,
+        &self,
         camera: Camera,
         tx: Sender<Box<PixelsBuffer>>,
     ) {
@@ -74,4 +75,66 @@ pub trait Scene {
 
         println!("Time elapsed in expensive_function() is: {:?}", duration);
     }
+}
+
+pub fn get_pixels<const W: usize, const H: usize, const STEP: usize, S>(
+    scene: Arc<Box<S>>,
+    camera: Camera,
+    tx: Sender<Box<PixelsBuffer>>,
+) where
+    S: Scene,
+{
+    let aspect_ratio: Unit = (W as Unit) / (H as Unit);
+
+    let direction = camera.direction();
+    let r = camera.r();
+    let u = camera.u();
+    let start = Instant::now();
+    let fov_scale = camera.fov_scale
+
+    let scene = Arc::new(Box::new(scene));
+
+    let handles = (0..H).map(|row| {
+        let scene = scene.clone();
+        thread::spawn(move || {
+            for row in (0..H).step_by(STEP) {
+                for col in (0..W).step_by(STEP) {
+                    let mut rng = rand::thread_rng();
+                    let mut pb = Box::new(PixelsBuffer::new(row, col));
+                    for y in row..(row + STEP) {
+                        for x in col..(col + STEP) {
+                            let adj_y: Unit = (y as Unit) / ((H as Unit) / 2.) - 1.;
+                            let adj_x: Unit =
+                                ((x as Unit) / ((W as Unit) / 2.) - 1.) * aspect_ratio;
+
+                            let d = Vec3(
+                                direction.0
+                                    + (r.0 * nalgebra::Vector1::new(camera.fov_scale() * adj_x))
+                                    + (u.0 * (-camera.fov_scale() * adj_y)),
+                            );
+
+                            let mut colors = [BLACK; ITERATIONS];
+                            for chunk in colors.chunks_mut(8) {
+                                for color in chunk {
+                                    *color = scene.compute_color(&camera.origin, &d, &mut rng);
+                                }
+                            }
+
+                            let c = colors.to_vec().avg().into();
+                            pb.pixels[(y - row) * STEP + (x - col)] = c;
+                        }
+                    }
+                    tx.send(pb).unwrap();
+                }
+            }
+        })
+    });
+
+    for handle in handles {
+        handle.join();
+    }
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
